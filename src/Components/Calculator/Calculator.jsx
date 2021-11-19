2eec9ee915ea4eb0fb1c6141736b928431c13ada
Karl Cereno <karlcereno@gmail.com>
2021-11-19 02:19:58 -0500

    Add dynamic Actual Weight logic

--- src/Components/Calculator/Calculator.jsx
+++ src/Components/Calculator/Calculator.jsx
@@ -2,8 +2,14 @@ import classes from "./Calculator.module.scss";
 import BarbellButtons from "./BarbellButtons/BarbellButtons";
 import PlateSet from "./PlateSet/PlateSet";
 import Breakdown from "./Breakdown/Breakdown";
+import { useContext } from "react";
+import AppContext from "../../store/AppContext";
 
 export default function Calculator() {
+  const { barWeight, totalPlateWeight } = useContext(AppContext);
+
+  let totalWeight = barWeight + totalPlateWeight;
+
   return (
     <div className={classes.container}>
       <div>
@@ -14,7 +20,7 @@ export default function Calculator() {
         <h3>Target Weight</h3>
         <input type="number"></input>
         <h3>Actual Weight</h3>
-        <h4>250 lbs</h4>
+        <h4>{totalWeight} lbs</h4>
         <h3>Breakdown</h3>
         <Breakdown />
         {/* Weight Input */}
